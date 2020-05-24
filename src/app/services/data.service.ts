02104218f72a5c0b50f03830f43c8c16b6834a95
lutfioz <luttfioz@gmail.com>
2020-05-24 23:25:39 +0300

    Add data service to core service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CoreService } from './core.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataService {
   cities: any[];
   private readonly baseURL = 'https://restcountries.eu/rest/v2/all';
 
-  constructor(public http: HttpClient) {
+  constructor(public http: HttpClient, public coreService: CoreService) {
     // 671ac7a5d543c9935d09d1a2e6ebfb5b
     // this._commit([{ id: 1, name: 'Berlin' }, { id: 2, name: 'London' }]);
     const cities: any[] = JSON.parse(localStorage.getItem('cities')) || [];
@@ -25,6 +26,13 @@ export class DataService {
   add(city: any) {
     this.cities.push(new Object(city));
     this._commit(this.cities);
+    this.coreService.citiesState.next(this.cities);
+  }
+
+  remove(city: any) {
+    this.cities = this.cities.filter(_city => _city.id !== city.id);
+    this._commit(this.cities);
+    this.coreService.citiesState.next(this.cities);
   }
 
   _commit(cities: any[]) {
