535910a344b5539395d1031bb0cedf98a87c4d9d
lutfioz <luttfioz@gmail.com>
2020-05-24 23:44:44 +0300

    Add remove method

--- src/app/ui/card/card.component.ts
+++ src/app/ui/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { DataService } from './../../services/data.service';
 
 @Component({
   selector: 'app-card',
@@ -10,10 +11,14 @@ export class CardComponent implements OnInit {
 
   @Input() city;
 
-  constructor() {
+  constructor(public dataService: DataService) {
   }
 
   ngOnInit() {
+  }
+
+  removeCity() {
+    this.dataService.remove(this.city);
   }
 
 }
